08330a89c955123251950ec184e263ab46fea238
Mohamed Zeamari <zeamarim@MeinApfel.local>
2022-06-25 19:29:32 +0200

    update

---
+++ Developpement/ImmutabilityExercise/ImmutabilityExercise/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  ImmutabilityExercise
+//
+//  Created by Mohamed Zeamari on 25/06/2022.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Developpement/Quizzler-iOS13/Quizzler-iOS13/Model/QuizzBrain.swift
@@ -0,0 +1,9 @@
+//
+//  QuizzBrain.swift
+//  Quizzler-iOS13
+//
+//  Created by Mohamed Zeamari on 24/06/2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
